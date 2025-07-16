079df18a6c0b94d3b88e8cf863d67b4c8a1264f0
VincenzoImp <vincenzoimperati99@gmail.com>
2025-07-16 16:32:41 +0200

    changed title and description

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "Nostreum",
+  description: "First client bridging Ethereum and Nostr",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
